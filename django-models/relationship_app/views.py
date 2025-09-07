68017a530c4d06b9f0628b19c4f2ee2d51498d55
Tsi2216 <getanehshimelis40@gmail.com>
2025-09-07 11:56:50 -0700

    added relationship app

---
+++ django-models/relationship_app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Book, Library
+
+def list_books(request):
+    """Function-based view to list all books."""
+    books = Book.objects.all()
+    context = {'books': books}
+    return render(request, 'list_books.html', context)
+
+class LibraryDetailView(DetailView):
+    """Class-based view to display a specific library's details."""
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
