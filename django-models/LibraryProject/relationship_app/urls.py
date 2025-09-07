3a5e6b08f99535eef75d0c89bca6b1db08c2c87b
Tsi2216 <getanehshimelis40@gmail.com>
2025-09-07 14:15:46 -0700

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import list_books, LibraryDetailView, register  # Include register here
+from . import views  # Import views
 
 urlpatterns = [
-    path('books/', list_books, name='list_books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('register/', register, name='register'),  # This line adds the register view
-    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('books/', views.list_books, name='list_books'),  # URL for the books list
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # URL for library detail
+    path('register/', views.register, name='register'),  # Add this line for the register view
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),  # URL for login
+    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),  # URL for logout
 ]
