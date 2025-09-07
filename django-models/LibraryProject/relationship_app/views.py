1a49fd1e2a15904a8e746869abed5d3b75d4fd4c
Tsi2216 <getanehshimelis40@gmail.com>
2025-09-07 13:08:38 -0700

    added new file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,15 +1,24 @@
+from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render
-from django.views.generic import DetailView
-from relationship_app.models import Book, Library
+from .models import UserProfile
 
-def list_books(request):
-    """Function-based view to list all books."""
-    books = Book.objects.all()
-    context = {'books': books}
-    return render(request, 'relationship_app/list_books.html', context)
+def is_admin(user):
+    return user.is_authenticated and user.userprofile.role == 'Admin'
 
-class LibraryDetailView(DetailView):
-    """Class-based view to display a specific library's details."""
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
+def is_librarian(user):
+    return user.is_authenticated and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.is_authenticated and user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'member_view.html')
