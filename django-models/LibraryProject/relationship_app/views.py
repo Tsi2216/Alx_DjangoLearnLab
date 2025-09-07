6bbe12b874ecfc73eac55c6a81e17a097000d5e1
Tsi2216 <getanehshimelis40@gmail.com>
2025-09-07 12:49:12 -0700

    added new file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library # This line imports both models
-
-def list_books(request):
-    """Function-based view to list all books."""
-    books = Book.objects.all()
-    context = {'books': books}
-    return render(request, 'relationship_app/list_books.html', context)
+from .models import Book, Library  # Make sure this import is included
 
 class LibraryDetailView(DetailView):
-    """Class-based view to display a specific library's details."""
+    """Class-based view to display a specific library's details and list all books."""
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+    def get_context_data(self, **kwargs):
+        """Add books related to the library to the context."""
+        context = super().get_context_data(**kwargs)
+        context['books'] = Book.objects.filter(library=self.object)  # Assuming Book has a ForeignKey to Library
+        return context
