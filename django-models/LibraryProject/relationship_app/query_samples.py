ea4a7edd9511aace0692366e3b6ffc9dd6bf9953
Tsi2216 <getanehshimelis40@gmail.com>
2025-09-07 11:42:18 -0700

    added relationship app

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -25,8 +25,8 @@ def list_books_in_library(library_name):
 def retrieve_librarian_for_library(library_name):
     """Retrieve the librarian for a library."""
     try:
-        library = Library.objects.get(name=library_name)
-        librarian = library.librarian
+        library_obj = Library.objects.get(name=library_name)
+        librarian = Librarian.objects.get(library=library_obj)
         print(f"The librarian for {library_name} is: {librarian.name}")
     except Library.DoesNotExist:
         print(f"Library '{library_name}' not found.")
