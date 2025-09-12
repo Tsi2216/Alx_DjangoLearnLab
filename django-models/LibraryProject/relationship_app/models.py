0178bfafb2daae93962fd8edb16f26ad876c107c
Tsi2216 <getanehshimelis40@gmail.com>
2025-09-12 10:03:38 -0700

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,18 +1,35 @@
-# models.py
 from django.db import models
-from django.contrib.auth.models import User
 
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    published_date = models.DateField()
+# Create your models here.
 
-    class Meta:
-        permissions = [
-            ('can_add_book', 'Can add book'),
-            ('can_change_book', 'Can change book'),
-            ('can_delete_book', 'Can delete book'),
-        ]
+class Author(models.Model):
+    """
+    A model to represent a book's author.
+    
+    This class defines the fields for an Author, such as their name.
+    The `__str__` method is overridden to provide a human-readable representation
+    of the object, which is useful in the Django admin interface.
+    """
+    name = models.CharField(max_length=100, help_text="The name of the author.")
 
     def __str__(self):
+        """
+        Returns the string representation of the Author object.
+        """
+        return self.name
+
+class Book(models.Model):
+    """
+    A model to represent a book.
+    
+    This class includes a ForeignKey field to link a book to its author.
+    """
+    title = models.CharField(max_length=200, help_text="The title of the book.")
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, help_text="The author of the book.")
+    publication_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        """
+        Returns the string representation of the Book object.
+        """
         return self.title
