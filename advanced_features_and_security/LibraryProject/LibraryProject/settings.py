549eec4ef6840e4ec3676f8e129e79768923fcb3
Tsi2216 <getanehshimelis40@gmail.com>
2025-09-13 08:25:44 -0700

    adde settting.py

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -111,3 +111,6 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# New line to specify the custom user model 
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
