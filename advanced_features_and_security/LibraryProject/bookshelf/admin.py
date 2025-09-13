80e0948a11fa50182fcab8b6f5c5c9e12817af1e
Tsi2216 <getanehshimelis40@gmail.com>
2025-09-13 08:19:24 -0700

    updated admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,6 +1,15 @@
-# bookshelf/admin.py
 from django.contrib import admin
-from .models import Book, Review
+from django.contrib.auth.admin import UserAdmin
+from .models import Book, Review, CustomUser
 
+class CustomUserAdmin(UserAdmin):
+    # Add the custom fields to the fieldsets
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    # Add the custom fields to the list_display
+    list_display = ('username', 'email', 'date_of_birth', 'is_staff')
+
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Book)
 admin.site.register(Review)
