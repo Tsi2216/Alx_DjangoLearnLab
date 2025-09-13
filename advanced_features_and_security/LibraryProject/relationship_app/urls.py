bd9ddcc4320e388088984e5cc9e12d660901c0fb
Tsi2216 <getanehshimelis40@gmail.com>
2025-09-13 08:15:27 -0700

    updated models.py

--- advanced_features_and_security/LibraryProject/relationship_app/urls.py
+++
@@ -1,9 +0,0 @@
-# urls.py
-from django.urls import path
-from .views import add_book, edit_book, delete_book
-
-urlpatterns = [
-    path('add_book/', add_book, name='add_book'),  # URL for adding a book
-    path('edit_book/<int:pk>/', edit_book, name='edit_book'),  # URL for editing a book
-    path('delete_book/<int:pk>/', delete_book, name='delete_book'),  # URL for deleting a book
-]
