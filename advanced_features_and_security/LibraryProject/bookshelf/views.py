ef5396f2e6897619f15a38d3d19c660955c1d85e
Tsi2216 <getanehshimelis40@gmail.com>
2025-09-14 12:51:55 -0700

    added new file

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required, permission_required
 from .models import Book, Review
-from .forms import BookForm, ReviewForm, ExampleForm   # ✅ Added ExampleForm
+from .forms import ExampleForm
 
 @login_required
 @permission_required('bookshelf.can_view', raise_exception=True)
