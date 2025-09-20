8b064aeb2589383600cd622a8fa5767edf0761e9
Tsi2216 <getanehshimelis40@gmail.com>
2025-09-20 11:19:33 -0700

    updated file

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)   # field: title
+    author = models.CharField(max_length=100)  # field: author
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
--- api_project/api_project/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Book(models.Model):
-    title = models.CharField(max_length=200)   # required "title"
-    author = models.CharField(max_length=100)  # required "author"
-
-    def __str__(self):
-        return f"{self.title} by {self.author}"
