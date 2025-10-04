9d903dc29e491884d62c0bdb8473ed917b812c29
Tsi2216 <getanehshimelis40@gmail.com>
2025-10-04 12:57:54 -0700

    added new file

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,9 +1,16 @@
 # blog/views.py
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from .models import Post
 
+# -----------------------
+# BLOG POST CRUD VIEWS
+# -----------------------
+
+# List all posts
 class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
@@ -11,11 +18,13 @@ class PostListView(ListView):
     ordering = ['-published_date']
 
 
+# View a single post
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
 
 
+# Create a new post (authenticated users only)
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content']
@@ -26,6 +35,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
+# Update an existing post (only the author can edit)
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
@@ -40,6 +50,7 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         return self.request.user == post.author
 
 
+# Delete a post (only the author can delete)
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = 'blog/post_confirm_delete.html'
@@ -48,3 +59,13 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     def test_func(self):
         post = self.get_object()
         return self.request.user == post.author
+
+
+# -----------------------
+# OPTIONAL: Dashboard Example
+# -----------------------
+@login_required
+def user_dashboard(request):
+    """Optional view to show posts by the logged-in user."""
+    posts = Post.objects.filter(author=request.user)
+    return render(request, 'blog/dashboard.html', {'posts': posts})
