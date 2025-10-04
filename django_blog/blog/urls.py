4b0502f3371db64a248a4010e0a21d39bad6b9a0
Tsi2216 <getanehshimelis40@gmail.com>
2025-10-04 12:55:56 -0700

    added new file

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,19 +1,16 @@
+# blog/urls.py
 from django.urls import path
 from . import views
 
-app_name = 'blog'
-
 urlpatterns = [
-    # authentication
-    path('register/', views.register_view, name='register'),
-    path('login/', views.CustomLoginView.as_view(), name='login'),
-    path('logout/', views.CustomLogoutView.as_view(), name='logout'),
-    path('profile/', views.profile_view, name='profile'),
+    # Home and Post List
+    path('', views.PostListView.as_view(), name='post-list'),
 
-    # posts CRUD
-    path('posts/', views.PostListView.as_view(), name='post_list'),
-    path('posts/new/', views.PostCreateView.as_view(), name='post_create'),
-    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
-    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_update'),
-    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
+    # Post Details
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+
+    # Create, Update, and Delete Posts
+    path('post/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,112 +1,49 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.urls import reverse_lazy, reverse
-from django.http import Http404
-from django.contrib import messages
-from django.contrib.auth import login
-from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth.decorators import login_required
+# blog/views.py
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-
+from django.urls import reverse_lazy
 from .models import Post
-from .forms import UserRegistrationForm, UserUpdateForm, ProfileForm, PostForm
-
-# Registration
-def register_view(request):
-    if request.user.is_authenticated:
-        return redirect('blog:profile')
-    if request.method == 'POST':
-        form = UserRegistrationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            messages.success(request, "Registration successful. Welcome!")
-            return redirect('blog:profile')
-        else:
-            messages.error(request, "Please correct the errors below.")
-    else:
-        form = UserRegistrationForm()
-    return render(request, 'blog/register.html', {'form': form})
-
-class CustomLoginView(LoginView):
-    template_name = 'blog/login.html'
-    redirect_authenticated_user = True
-
-class CustomLogoutView(LogoutView):
-    template_name = 'blog/logout.html'
-
-@login_required
-def profile_view(request):
-    user = request.user
-    if request.method == 'POST':
-        u_form = UserUpdateForm(request.POST, instance=user)
-        p_form = ProfileForm(request.POST, request.FILES, instance=user.profile)
-        if u_form.is_valid() and p_form.is_valid():
-            u_form.save()
-            p_form.save()
-            messages.success(request, "Your profile has been updated.")
-            return redirect('blog:profile')
-        else:
-            messages.error(request, "Please correct the errors below.")
-    else:
-        u_form = UserUpdateForm(instance=user)
-        p_form = ProfileForm(instance=user.profile)
-    return render(request, 'blog/profile.html', {'u_form': u_form, 'p_form': p_form})
-
-# Post CRUD views
 
 class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
-    paginate_by = 10
+    ordering = ['-published_date']
 
-    def get_queryset(self):
-        # show published posts for public, but allow authors to see their drafts via detail view
-        return Post.objects.filter(status='published')
 
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
-    context_object_name = 'post'
 
-    def get_object(self, queryset=None):
-        obj = super().get_object(queryset)
-        # prevent other users from seeing drafts
-        if obj.status == 'draft' and self.request.user != obj.author:
-            raise Http404("Not found")
-        return obj
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
-    form_class = PostForm
+    fields = ['title', 'content']
     template_name = 'blog/post_form.html'
-    extra_context = {'action': 'create'}
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
-    def get_success_url(self):
-        return reverse('blog:post_detail', kwargs={'pk': self.object.pk})
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
-    form_class = PostForm
+    fields = ['title', 'content']
     template_name = 'blog/post_form.html'
-    extra_context = {'action': 'update'}
 
-    def get_success_url(self):
-        return reverse('blog:post_detail', kwargs={'pk': self.object.pk})
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
     def test_func(self):
         post = self.get_object()
         return self.request.user == post.author
 
+
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = 'blog/post_confirm_delete.html'
-    success_url = reverse_lazy('blog:post_list')
+    success_url = reverse_lazy('post-list')
 
     def test_func(self):
         post = self.get_object()
