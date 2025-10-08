f8a7212326dd84eed1343de2a0139e4a1f288881
Tsi2216 <getanehshimelis40@gmail.com>
2025-10-08 03:32:55 -0700

    modified file

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -13,19 +13,35 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         # Write permissions are only allowed for the owner of the object
         return obj.author == request.user
 
+
 class PostViewSet(viewsets.ModelViewSet):
     """ViewSet for managing posts."""
-    queryset = Post.objects.all().order_by('-created_at')
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     filter_backends = [filters.SearchFilter]
-    search_fields = ['title', 'content']  # Search by title or content
+    search_fields = ['title', 'content']
+
+    def get_queryset(self):
+        """Return posts from users the current user is following, or all posts for anonymous users."""
+        user = self.request.user
+        if user.is_authenticated:
+            # Get users the current user is following
+            following_users = user.following.all()  # Make sure your User model has 'following' ManyToMany
+            return Post.objects.filter(author__in=following_users).order_by('-created_at')
+        # For anonymous users, return all posts
+        return Post.objects.all().order_by('-created_at')
 
     def perform_create(self, serializer):
         """Save the post with the current user as the author."""
         serializer.save(author=self.request.user)
 
+
 class CommentViewSet(viewsets.ModelViewSet):
     """ViewSet for managing comments."""
     queryset = Comment.objects.all().order_by('-created_at')
-    serializer
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        """Save the comment with the current user as the author."""
+        serializer.save(author=self.request.user)
