3474fe15b34d27d89f94865ca67599d48ab561f9
Tsi2216 <getanehshimelis40@gmail.com>
2025-10-08 02:16:50 -0700

    modified file

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,31 +1,71 @@
-# accounts/serializers.py
-from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from rest_framework.authtoken.models import Token
+from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
 
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
-    followers_count = serializers.IntegerField(source='followers.count', read_only=True)
-    following_count = serializers.IntegerField(source='following.count', read_only=True)
-
+    """Serializer to display basic user info"""
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers_count', 'following_count']
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
+
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, min_length=8)
+    """
+    Serializer for user registration with password confirmation and validation.
+    Uses get_user_model().objects.create_user() to safely create users.
+    """
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        validators=[validate_password],
+        style={'input_type': 'password'}
+    )
+    password2 = serializers.CharField(
+        write_only=True,
+        required=True,
+        style={'input_type': 'password'}
+    )
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'email', 'password', 'password2']
+
+    def validate(self, attrs):
+        """Ensure both passwords match."""
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Passwords do not match."})
+        return attrs
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        """Create user using the custom manager (create_user)."""
+        validated_data.pop('password2')  # remove password2 since not in model
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
-            email=validated_data.get('email', ''),
+            email=validated_data.get('email'),
             password=validated_data['password']
         )
-        # create token
-        Token.objects.create(user=user)
         return user
+
+
+class ChangePasswordSerializer(serializers.Serializer):
+    """
+    Serializer for changing user password.
+    """
+    old_password = serializers.CharField(required=True, write_only=True)
+    new_password = serializers.CharField(
+        required=True,
+        write_only=True,
+        validators=[validate_password]
+    )
+
+
+class LoginSerializer(serializers.Serializer):
+    """Serializer for login endpoint (manual token auth)."""
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        style={'input_type': 'password'}
+    )
